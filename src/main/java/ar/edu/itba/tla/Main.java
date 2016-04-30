f5d03f798a68b3bf2f5acd1cbe64785644605b65
Juan Pablo Orsay <juan.orsay@avature.net>
2016-04-30 13:24:01 -0300

    add commons-cli for cli argument parsing

--- src/main/java/ar/edu/itba/tla/Main.java
+++ src/main/java/ar/edu/itba/tla/Main.java
@@ -1,7 +1,26 @@
 package ar.edu.itba.tla;
 
+import org.apache.commons.cli.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Jello world!");
+        CommandLineParser parser = new DefaultParser();
+        Options options = getCommandLineOptions();
+        HelpFormatter formatter = new HelpFormatter();
+        try {
+            CommandLine line = parser.parse(options, args);
+            if (line.hasOption("h")) {
+                formatter.printHelp("cafe", options);
+            }
+        } catch (ParseException e) {
+            formatter.printHelp("cafe", options);
+        }
+    }
+
+    private static Options getCommandLineOptions() {
+        Options options = new Options();
+        options.addOption("d", "debug", false, "print debugging information");
+        options.addOption("h", "help", false, "print help");
+        return options;
     }
 }

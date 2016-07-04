519b5b39d61ba992904a66bc4c88b4a85a9509c7
Qcho <qcho@avature.net>
2016-07-04 02:46:45 -0300

    write

--- src/atlc/nodes/FunctionFactory.java
+++ src/atlc/nodes/FunctionFactory.java
@@ -9,11 +9,19 @@ import java.util.function.Function;
 public final class FunctionFactory {
 
     public static void writeLine(Function<Context, Type> expr, Context context) {
+        write(expr, context, true);
+    }
+
+    public static void write(Function<Context, Type> expr, Context context) {
+        write(expr, context, false);
+    }
+
+    private static void write(Function<Context, Type> expr, Context context, boolean newLine) {
         context.getGa().getStatic(Type.getType(System.class), "out", Type.getType(PrintStream.class));
         Type type = expr.apply(context);
         context.getGa().invokeVirtual(
                 Type.getType(PrintStream.class),
-                new Method("println", Type.VOID_TYPE, new Type[] {type})
+                new Method(newLine ? "println" : "print", Type.VOID_TYPE, new Type[] {type})
         );
     }
 

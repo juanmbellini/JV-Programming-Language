0da520249035b17f64c112d0759d090644c3f4cc
Qcho <qcho@avature.net>
2016-07-04 22:29:28 -0300

    add if-else first impl

---
+++ src/atlc/nodes/FlowControlFactory.java
@@ -0,0 +1,38 @@
+package atlc.nodes;
+
+import atlc.Context;
+import org.objectweb.asm.Label;
+import org.objectweb.asm.Type;
+import org.objectweb.asm.commons.GeneratorAdapter;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class FlowControlFactory {
+    public static Consumer<Context> createIf (
+            Function<Context, Type> expr,
+            Consumer<Context> ifTrueBlock,
+            Consumer<Context> elseBlock
+    ) {
+        return context -> {
+            GeneratorAdapter ga = context.getGa();
+            final Label ifTrueRun = ga.newLabel();
+            final Label elseRun = ga.newLabel();
+            final Label endIf = ga.newLabel();
+
+            Type type = expr.apply(context);
+            // TODO: use ga.ifCmp(type, ...);
+            ga.ifZCmp(GeneratorAdapter.NE, ifTrueRun);
+            ga.goTo(elseRun);
+
+            ga.visitLabel(ifTrueRun);
+            ifTrueBlock.accept(context);
+            ga.goTo(endIf);
+            ga.visitLabel(elseRun);
+            if (elseBlock != null) {
+                elseBlock.accept(context);
+            }
+            ga.visitLabel(endIf);
+        };
+    }
+}

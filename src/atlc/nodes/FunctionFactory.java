e8dd5eefd23a4a447a4edb78c0882a70e564a420
Juan Pablo Orsay <juan.orsay@avature.net>
2016-07-05 01:47:37 -0300

    read almost working

--- src/atlc/nodes/FunctionFactory.java
+++ src/atlc/nodes/FunctionFactory.java
@@ -3,7 +3,11 @@ package atlc.nodes;
 import atlc.Context;
 import org.objectweb.asm.commons.Method;
 import org.objectweb.asm.Type;
+
+import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.Scanner;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 public final class FunctionFactory {
@@ -16,6 +20,25 @@ public final class FunctionFactory {
         write(expr, context, false);
     }
 
+    public static void read(Function<Context, Type> targetType, Consumer<Context> target, Context context) {
+        context.getGa().newInstance(Type.getType(Scanner.class));
+        context.getGa().dup(); // Duplicate since we are constructing and calling
+        context.getGa().getStatic(Type.getType(System.class), "in", Type.getType(InputStream.class));
+        context.getGa().invokeConstructor(Type.getType(Scanner.class), Method.getMethod("void <init> (java.io.InputStream)"));
+        Type type = targetType.apply(context);
+        context.getGa().invokeVirtual(Type.getType(Scanner.class), getReadMethod(type));
+    }
+
+    private static Method getReadMethod(Type type) {
+        if (type.equals(Type.getType(Integer.class)) || type.equals(Type.INT_TYPE)) {
+            return new Method("nextInt", Type.INT_TYPE, new Type[] {});
+        }
+        if (type.equals(Type.getType(Boolean.TYPE)) || type.equals(Type.BOOLEAN_TYPE)) {
+            return new Method("nextBoolean", Type.BOOLEAN_TYPE, new Type[] {});
+        }
+        return new Method("next", Type.getType(String.class), new Type[] {});
+    }
+
     private static void write(Function<Context, Type> expr, Context context, boolean newLine) {
         context.getGa().getStatic(Type.getType(System.class), "out", Type.getType(PrintStream.class));
         Type type = expr.apply(context);
@@ -24,5 +47,4 @@ public final class FunctionFactory {
                 new Method(newLine ? "println" : "print", Type.VOID_TYPE, new Type[] {type})
         );
     }
-
 }
